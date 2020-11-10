1d2a9e3dd13ea95984f7088f3421a47c861060f7
fight1991 <359193813@qq.com>
2020-11-10 16:11:30 +0800

    登录界面暂存

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,11 @@ class MyApp extends StatelessWidget {
       title: 'FoxESS',
       home: Home(),
       theme: ThemeData(
-        primaryColor: Color.fromRGBO(4,60,97, 1),
+        primaryColor: Colors.white,
         textTheme: TextTheme(
           bodyText1: TextStyle(color: Colors.white60)
         ),
-        scaffoldBackgroundColor: Color.lerp(Color.fromRGBO(0,67,107,1), Color.fromRGBO(2,36,59,1), 0.5)
+        // scaffoldBackgroundColor: Color.lerp(Color.fromRGBO(0,67,107,1), Color.fromRGBO(2,36,59,1), 0.5)
       ),
     );
   }
@@ -22,11 +22,29 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('login')
+        elevation: 0.0
       ),
-      body: Center(
-        child: Text('登录'),
-      ),
+      body: ConstrainedBox(
+        constraints: BoxConstraints.expand(),
+        child: Stack(
+          children: <Widget>[
+            Positioned(
+              bottom: 80.0,
+              right: 20.0,
+              child: Container(
+                child: Text('hah'),
+                width: 250.0,
+                height: 400.0,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [Colors.blue, Colors.white]
+                  )
+                ),
+              ),
+            )
+          ]
+        ),
+      )
     );
   }
 }
