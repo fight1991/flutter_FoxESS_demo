c956edac408dc1ee5fd0f24dc2399d5b34c6ae1d
fight1991 <359193813@qq.com>
2020-11-12 16:34:56 +0800

    container自适应暂存

--- lib/main.dart
+++ lib/main.dart
@@ -24,50 +24,91 @@ class Home extends StatelessWidget {
       appBar: AppBar(
         elevation: 0.0
       ),
-      body: ConstrainedBox(
-        constraints: BoxConstraints.expand(),
-        child: Stack(
-          children: <Widget>[
-            Positioned(
-              top: 0,
-              right: 0,
+      body: Column(
+        children: <Widget>[
+          Align(
+            alignment: Alignment.centerRight,
+            child: Padding(
+              padding: EdgeInsets.only(right: 20.0, bottom: 80.0),
               child: Image(
                 image: AssetImage("images/login_logo.png"),
                 fit: BoxFit.cover,
-                width: 100.0,
-                height: 30.0,
+                width: 180.0,
               ),
-            ),
-            Positioned(
-              bottom: 0,
-              left: 0,
-              child: Container(
-                width: MediaQuery.of(context).size.width,
-                height: 380.0,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(topLeft: Radius.circular(150.0)),
-                  color: Color(0xfff6f6f6),
-                  boxShadow: [BoxShadow(color: Colors.black54, offset: Offset(5.0, 5.0), blurRadius: 10.0, spreadRadius: 2.0)],
-                ),
-              )
-            ),
-            Positioned(
-              bottom: 80.0,
-              right: 20.0,
-              child: Container(
-                child: Text('hah'),
-                width: 280.0,
-                height: 400.0,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20.0),
-                  gradient: LinearGradient(
-                    colors: [Color(0xff325CAF), Color(0xff6CA0D1)]
+            )
+          ),
+          Expanded(
+            child: ConstrainedBox(
+              constraints: BoxConstraints.expand(),
+              child: Stack(
+                overflow: Overflow.visible,
+                fit: StackFit.expand, // 针对没有定位的元素撑满stack, container没有子元素会自动撑满父元素
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.only(top: 25.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(topLeft: Radius.circular(150.0)),
+                      color: Color(0xfff6f6f6),
+                      boxShadow: [BoxShadow(color: Colors.black54, offset: Offset(5.0, 5.0), blurRadius: 10.0, spreadRadius: 2.0)],
+                    ),
+                  ),
+                  Positioned(
+                    top: 0.0,
+                    right: 20.0,
+                    child: Container(
+                      child: Column(children: <Widget>[
+                        TextField(
+                          decoration: InputDecoration(
+                              labelText: "用户名",
+                              hintText: "用户名或邮箱",
+                              prefixIcon: Icon(Icons.person)
+                          ),
+                        ),
+                        TextField(
+                          decoration: InputDecoration(
+                              labelText: "用户名",
+                              hintText: "用户名或邮箱",
+                              prefixIcon: Icon(Icons.person)
+                          ),
+                        ),
+                        TextField(
+                          decoration: InputDecoration(
+                              labelText: "密码",
+                              hintText: "您的登录密码",
+                              prefixIcon: Icon(Icons.lock)
+                          ),
+                          obscureText: true,
+                        ),
+                      ],),
+                      width: 280.0,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20.0),
+                        gradient: LinearGradient(
+                          colors: [Color(0xff325CAF), Color(0xff6CA0D1)]
+                        )
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    left: 10.0,
+                    bottom: 10.0,
+                    child: Transform(
+                      transform: Matrix4.identity()
+                        ..rotateZ(270 * 3.1415927 / 180),
+                      child: Row(
+                        children: <Widget>[
+                          FlatButton(child: Text('FORGET PASSWORD'), onPressed: (){print('forget');}),
+                          FlatButton(child: Text('SING UP'), onPressed: (){print('sign up');}),
+                          FlatButton(child: Text('SING IN'), onPressed: (){print('sign in');}),
+                        ],
+                      )
+                    )
                   )
-                ),
+                ]
               ),
             ),
-          ]
-        ),
+          ),
+        ],
       )
     );
   }
