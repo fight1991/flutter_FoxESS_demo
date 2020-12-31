c8c9a2415965f110f0b31472af1bb7b1eea7ac5e
fight1991 <359193813@qq.com>
2020-12-31 17:09:19 +0800

    电站页面暂存

--- lib/pages/overview/Station.dart
+++ lib/pages/overview/Station.dart
@@ -3,13 +3,20 @@ class Station extends StatefulWidget {
   @override
   _Station createState() => _Station();
 }
-class _Station extends State<Station> {
+class _Station extends State<Station> with SingleTickerProviderStateMixin{
+  TabController _tabController;
+  List tabs = ['全部', '正常', '异常', '离线'];
   @override
+  void initState() {
+    super.initState();
+    // 创建Controller  
+    _tabController = TabController(length: tabs.length, vsync: this);
+  }
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('电站'),
-        elevation: 0.0,
+        elevation: 10.0,
         centerTitle: true,
         leading: Text(''),
         actions: <Widget>[
@@ -17,6 +24,19 @@ class _Station extends State<Station> {
             padding: EdgeInsets.symmetric(horizontal:15.0),
             child: Icon(Icons.add),
           )
+        ],
+        bottom: TabBar(
+          tabs: tabs.map((v) => Text(v)).toList(),
+          controller: _tabController,
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: <Widget>[
+          Text('1'),
+          Text('2'),
+          Text('3'),
+          Text('4')
         ],
       ),
     );
